7149a5ab4a46542dafd8e2906fb947ddda4002cb
IsraMejia <ihmaisrael@gmail.com>
2020-11-26 21:50:50 -0600

    Se creo tooooda la data que se mandara a renderizar en tarjetas

---
+++ lib/rappi_concept.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const _backgroundColor = Color(0xFFF6F9FA);
+const _blueColor = Color(0xFF0D1863);
+const _greenColor = Color(0xFF2BBEBA);
+
+class RappiConcept extends StatelessWidget {
+  
+//--------voy en 3:21 , replicar toda la data fue un relajo jeje
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Text('Salu2') ,
+        )
+      ),
+    );
+  }
+}
